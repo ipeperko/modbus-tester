efebc20bc945b5ef863539decec10849a9d495a1
ivo <ivo.peperko@i13tech.com>
2020-10-08 20:10:10 +0200

    Hello message with libmodbus version

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QDebug>
+#include <modbus/modbus.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    qDebug() << "modbus_tester - libmodbus version" << LIBMODBUS_VERSION_STRING;
 
     mainwindow win;
     win.show();
